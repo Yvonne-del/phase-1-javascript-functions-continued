67e642f92a423d475861adbeb0768f5ce8772d22
Yvonne-del <ys359375@gmail.com>
2025-03-09 17:52:47 +0300

    function expressions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+const mondayWork = function(what = "go to the office"){
+    return `This Monday, I will ${what}.`;
+}
+
+function wrapAdjective(string = "*"){
+    return function(state="special"){
+        return `You are ${string}${state}${string}!`;
+    }
+}
